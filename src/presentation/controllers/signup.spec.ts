92e14309f0c4ea702bf896bbbef88dfb15b5b1f0
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-07 09:40:06 -0300

    test: ensureSignUpController return an error if no name is provided

---
+++ src/presentation/controllers/signup.spec.ts
@@ -0,0 +1,20 @@
+import { SignUpController } from './signup'
+
+describe('SignUp Controller', () => {
+  // validar se os parâmetros estão vindo corretos
+
+  // se nome não for enviado returna 400
+  test('Should return 400 if no name is provided', () => {
+    const sut = new SignUpController()
+    const httpRequest = {
+      body: {
+        email: 'any_email@mail.com',
+        password: 'password',
+        passwordConfirmation: 'password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
+  })
+})
