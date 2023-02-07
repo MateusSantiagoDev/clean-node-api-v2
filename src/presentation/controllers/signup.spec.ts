9d2928bf0d54fdd3a6940d09b6524cf22e10df44
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-07 10:28:40 -0300

    refactor: ensure SignUpController.handle implement correct interfaces

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -33,6 +33,7 @@ describe('SignUp Controller', () => {
     expect(httpResponse.body).toEqual(new Error('Missing param: email'))
   })
 
+  // se password não for enviado returna 400
   test('Should return 400 if no password is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
@@ -46,7 +47,8 @@ describe('SignUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing param: password'))
   })
-  
+
+  // se passwordConfirmation não for enviado returna 400
   test('Should return 400 if no password confirmation is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
---
+++ src/presentation/protocols/http.ts
@@ -0,0 +1,8 @@
+export interface httpResponse {
+  statusCode: 400
+  body: any
+}
+
+export interface httpRequest {
+  body?: any
+}
