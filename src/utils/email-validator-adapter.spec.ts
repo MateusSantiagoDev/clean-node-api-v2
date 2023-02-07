1364ccae8b06408ae040cb2a933ee1cbd51ca6c2
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-07 19:38:59 -0300

    refactor: move sut cration to a factory helper method

--- src/utils/email-validator-adapter.spec.ts
+++ src/utils/email-validator-adapter.spec.ts
@@ -7,10 +7,14 @@ jest.mock('validator', () => ({
   }
 }))
 
+const makeSut = (): EmailValidatorAdapter => {
+  return new EmailValidatorAdapter()
+}
+
 describe('EmailValidator Adapter', () => {
   // se a biblioteca returnar false o teste tbm vai retornar false
   test('Should return false if validator returns false', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     // mocando o return do isEmail para false pra satisfazer o teste
     jest.spyOn(validator, 'isEmail').mockReturnValueOnce(false)
     const isValid = sut.isValid('invalid_email@mail.com')
@@ -19,14 +23,14 @@ describe('EmailValidator Adapter', () => {
 
   // se a biblioteca returnar true o teste tbm vai retornar true
   test('Should return true if validator returns true', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isValid = sut.isValid('valid_email@mail.com')
     expect(isValid).toBe(true)
   })
 
   // teste para garantir que o validator vai ser chamado como email correto
   test('Should call validator with correct email', () => {
-    const sut = new EmailValidatorAdapter()
+    const sut = makeSut()
     const isEmailSpy = jest.spyOn(validator, 'isEmail')
     sut.isValid('valid_email@mail.com')
     // toHaveBeenCalledWith verificando se o test vai receber o email valido
