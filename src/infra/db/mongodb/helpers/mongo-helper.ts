f95c15c1be90689d626528a192bf5e8d2e172955
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-08 16:38:23 -0300

    refactor: mongoHelper

--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,12 +1,14 @@
 import { MongoClient, Collection } from 'mongodb'
-import { Account } from '../../../../domain/model/account'
 
 // basicamente esse obj MongoHelper connecta e desconecta com o banco
 export const MongoHelper = {
   client: null as MongoClient,
 
   async connect (url: string): Promise<void> {
-    this.client = await MongoClient.connect(process.env.MONGO_URL)
+    this.client = await MongoClient.connect(process.env.MONGO_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    })
   },
 
   async disconnect (): Promise<void> {
