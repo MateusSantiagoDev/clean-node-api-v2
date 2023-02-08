26b241a34fa7d18b8e4ddcbfa03eb3e7bffbc65d
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-08 00:37:26 -0300

    test: ensure BcryptAdapter returns a hash on sucess

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,6 +1,13 @@
 import bcrypt from 'bcrypt'
 import { BcryptAdapter } from './bcrypt-adapter'
 
+// mocando o retorno do bcrypt e setando um valor
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise(resolve => resolve('hash'))
+  }
+}))
+
 describe('Bcrypt Adapter', () => {
   // teste para garantir q o bcrypterAdapter chame a biblioteca
   // bcrypt passando os parâmetros corretor
@@ -12,5 +19,13 @@ describe('Bcrypt Adapter', () => {
     await sut.encrypt('any_value')
     // esperamos que ele seja chamado com 'any_value' e estou setando o salt
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+
+  // se o bcrypt funcionar será retornanda a hashed gerada por ele
+  test('Should return a hash on sucess', async () => {
+    const salt = 12
+    const sut = new BcryptAdapter(salt)
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hash')
   })
 })
