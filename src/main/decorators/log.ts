7fef6174d752bcbc2860b4e23cdaed02909af6aa
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-09 11:41:06 -0300

    feat: ensure LogControllerDecorator calls controller with correct values

---
+++ src/main/decorators/log.ts
@@ -0,0 +1,26 @@
+import { Controller, HttpRequest, HttpResponse } from '../../presentation/protocols'
+
+// decorrator defoult que implementa o protocolo do controller
+// e esta adicionando uma camada a mais ao meu controllador
+// possibilitando que eu posso implementar mais métodos ao controlador
+// sem modificar o controlador
+export class LogControllerDecorator implements Controller {
+  private readonly controller: Controller
+  constructor (controller: Controller) {
+    this.controller = controller
+  }
+  // como as rotas na 'makeSignUpController' implementam um 'Controller'
+  // e o controller tem uma assinatura 'handle (httpRequest: HttpRequest):
+  // Promise <HttpResponse>' posso retornat o 'LogControllerDecorator'
+  // no lugar do 'makeSignUpController' pois tem a mesma assinatura
+
+  // dessa forma estou criando uma camanad a mais e tornando o meu
+  // método mais generico
+  async handle (httpRequest: HttpRequest): Promise <HttpResponse> {
+    await this.controller.handle(httpRequest)
+    // adicionando um método a mais no decorator e consequentemente
+    // ao 'SignUpController' pois esta sendo instanciado
+
+    return null // como estou usando o await na linha 20 posso retornar null
+  }
+}
