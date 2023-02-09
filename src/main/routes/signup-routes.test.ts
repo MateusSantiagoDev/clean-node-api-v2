1e4ac5d691b41c9ea58122518ced789fa9232d50
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-08 21:09:06 -0300

    refactor: connect to mangodb before integration test

--- src/main/routes/signup-routes.test.ts
+++ src/main/routes/signup-routes.test.ts
@@ -1,7 +1,23 @@
 import request from 'supertest'
 import app from '../config/app'
+import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
 
 describe('SignUp Routes', () => {
+  // para poder fazer os testes de integração
+  // é necessario realizar a conexão com o banco de dados
+  beforeAll(async () => {
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+
+  beforeEach(async () => {
+    const accountCollection = MongoHelper.getCollection('accounts')
+    await accountCollection.deleteMany({})
+  })
+
   test('Should return an account on sucess', async () => {
     await request(app)
       .post('/api/signup')
