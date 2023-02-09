22b4e270633ca9ee74aaab82fd030c4272ba0137
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-08 23:07:04 -0300

    feat: add SignUp Factory

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,5 +1,7 @@
 import { Router } from 'express'
 
+// depois de feita a conexão com o banco de dados agora é só fazer
+// a composição do objeto... e para isso vou usar as factorys
 export default (router: Router): void => {
   router.post('/signup', (req, res) => {
     res.json({ ok: 'ok' })
