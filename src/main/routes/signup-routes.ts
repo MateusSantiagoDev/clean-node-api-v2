9d4f0bea3efc0cd160847f6a371fe23497c1e785
Mateus Santiago <mateus_santiago2.3@outlook.com>
2023-02-08 23:28:38 -0300

    feat: add ExpressRoutAdapter

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,9 +1,9 @@
 import { Router } from 'express'
+import { makeSignUpController } from '../factories/signup'
+import { adaptRoute } from '../adapters/express-route-adapter'
 
 // depois de feita a conexão com o banco de dados agora é só fazer
 // a composição do objeto... e para isso vou usar as factorys
 export default (router: Router): void => {
-  router.post('/signup', (req, res) => {
-    res.json({ ok: 'ok' })
-  })
+  router.post('/signup', adaptRoute(makeSignUpController()))
 }
